a157303414487d5421a324015458be6ab6bc7d60
James Van Hinsbergh <info@jamesvanhinsbergh.co.uk>
2013-02-20 21:13:38 +0000

    Initial Build

---
+++ Testing.java
@@ -0,0 +1,12 @@
+public class Testing {
+
+
+	public void sayHello() {
+		System.out.println("Hello there!");
+	}
+
+	public void sayHelloTo(String name){
+		System.out.println("Hello" + name);
+	}
+
+}	
